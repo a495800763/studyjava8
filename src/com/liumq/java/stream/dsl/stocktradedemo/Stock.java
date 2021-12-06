2496f8910ceb29d5c5416a6b5fc86ecf3846d33d
liumq <495800763@qq.com>
2021-12-06 22:58:58 +0800

    自定义DSL bean

---
+++ src/com/liumq/java/stream/dsl/stocktradedemo/Stock.java
@@ -0,0 +1,25 @@
+package com.liumq.java.stream.dsl.stocktradedemo;
+
+/**
+ * 股票的bean
+ */
+public class Stock {
+    private String symbol;
+    private String market;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public String getMarket() {
+        return market;
+    }
+
+    public void setMarket(String market) {
+        this.market = market;
+    }
+}
