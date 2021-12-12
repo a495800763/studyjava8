c7562d434bc98f3020828adf7474c47410e9fbe6
liumq <495800763@qq.com>
2021-12-12 11:39:26 +0800

    temporalAdjuster

--- src/com/liumq/java/datedemo/Test.java
+++ src/com/liumq/java/datedemo/Test.java
@@ -3,16 +3,17 @@ package com.liumq.java.datedemo;
 import java.time.*;
 import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
+
 import static java.time.temporal.TemporalAdjusters.*;
 
 public class Test {
     public static void main(String[] args) {
 //        Date date = new Date(117,8,21);
 //        System.out.println(date);
-        parseDateTime();
+        useTemporalAdjuster();
     }
 
-    public static void userLocalDate(){
+    public static void userLocalDate() {
         LocalDate date = LocalDate.of(2017, 9, 21);
         System.out.println(date);
         int year = date.getYear();
@@ -26,7 +27,7 @@ public class Test {
         System.out.println(now);
     }
 
-    public static void  userLocalDate2(){
+    public static void userLocalDate2() {
         LocalDate date = LocalDate.now();
         int year = date.get(ChronoField.YEAR);
         int month = date.get(ChronoField.MONTH_OF_YEAR);
@@ -35,11 +36,11 @@ public class Test {
         int year1 = date.getYear();
         int monthValue = date.getMonthValue();
         int dayOfMonth = date.getDayOfMonth();
-        int a  = 1;
+        int a = 1;
 
     }
 
-    public static void userLocalTime(){
+    public static void userLocalTime() {
         LocalTime time = LocalTime.of(13, 45, 20);
         int hour = time.getHour();
         int minute = time.getMinute();
@@ -47,24 +48,24 @@ public class Test {
 
         LocalDate parseDate = LocalDate.parse("2021-11-21");
         LocalTime parseTime = LocalTime.parse("05:11:42");
-        int b = 1 ;
+        int b = 1;
     }
 
-    public static void createLocalDateTime(){
-        LocalDate date = LocalDate.of(2018,5,22);
-        LocalTime time =LocalTime.of(13,34,26);
+    public static void createLocalDateTime() {
+        LocalDate date = LocalDate.of(2018, 5, 22);
+        LocalTime time = LocalTime.of(13, 34, 26);
         LocalDateTime dt1 = LocalDateTime.of(2014, Month.SEPTEMBER, 21, 12, 45, 20);
         LocalDateTime dt2 = LocalDateTime.of(date, time);
         LocalDateTime dt3 = date.atTime(12, 56, 25);
         LocalDateTime dt4 = date.atTime(time);
         LocalDateTime dt5 = time.atDate(date);
 
-        int b =4;
+        int b = 4;
 
 
     }
 
-    public static void useInstantTime(){
+    public static void useInstantTime() {
         Instant instant1 = Instant.ofEpochSecond(3);
         Instant instant2 = Instant.ofEpochSecond(3, 0);
         Instant instant3 = Instant.ofEpochSecond(2, 1_000_000_000);
@@ -74,13 +75,13 @@ public class Test {
         int nano = now.getNano();
 
 
-        int q= 3;
+        int q = 3;
     }
 
     /**
      * 使用with 修改localdate和localtime
      */
-    public static void parseDateTime(){
+    public static void parseDateTime() {
         LocalDate date1 = LocalDate.of(2017, 9, 21);
         LocalDate date2 = date1.withYear(2021);
         LocalDate date3 = date1.withDayOfMonth(12);
@@ -92,7 +93,7 @@ public class Test {
     /**
      * 使用声明方式操纵localDate对象
      */
-    public static void  useDateTimeByDeclare(){
+    public static void useDateTimeByDeclare() {
         LocalDate date1 = LocalDate.of(2017, 9, 21);
         //date1时间加上一个星期
         LocalDate date2 = date1.plusWeeks(1);
@@ -101,5 +102,19 @@ public class Test {
         //date3时间加上6个月
         LocalDate date4 = date3.plus(6, ChronoUnit.MONTHS);
 
+    }
+
+    /**
+     * 使用预定义的TemporalAdjuster
+     */
+    public static void useTemporalAdjuster() {
+        LocalDate date1 = LocalDate.of(2014, 3, 18);
+        //下一个第一个周日
+        LocalDate date2 = date1.with(nextOrSame(DayOfWeek.SUNDAY));
+        //与date2同月的最后一日
+        LocalDate date3 = date2.with(lastDayOfMonth());
+        System.out.println(date1);
+        System.out.println(date2);
+        System.out.println(date3);
     }
 }
